4e2592ad51422bebac60a5298df181ee9f181c00
Veselin Slavchev <vs_slavchev@abv.bg>
2025-02-28 19:52:38 +0200

    Add spring project

---
+++ scheduledcalculations/src/main/java/com/slavchev/scheduledcalculations/ScheduledcalculationsApplication.java
@@ -0,0 +1,13 @@
+package com.slavchev.scheduledcalculations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduledcalculationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduledcalculationsApplication.class, args);
+	}
+
+}
